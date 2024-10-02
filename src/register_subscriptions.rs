9440a79b2cf6d6ad8f1d0efedea846973895ba08
Philipp Caspers <117186241+philipp-caspers@users.noreply.github.com>
2024-10-02 14:02:44 +0000

    feat: Add `tcp_pose` topic

--- src/register_subscriptions.rs
+++ src/register_subscriptions.rs
@@ -4,11 +4,12 @@ use rclrs::Node;
 
 use crate::{
     opc_ua_client::OPCUAClient,
-    ros_publisher::JointStatesBuffer,
+    ros_publisher::{JointStatesBuffer, TCPPoseBuffer},
 };
 
 pub fn register_opcua_subscriptions(ros_node: Arc<Node>, opc_ua_client: Arc<Mutex<OPCUAClient>>) {
     let ros_node_copy_joint_states_buffer = Arc::clone(&ros_node);
+    let ros_node_copy_tcp_pose_buffer = Arc::clone(&ros_node);
 
     let joint_states_buffer = Arc::new(Mutex::new(JointStatesBuffer::new(
         ros_node_copy_joint_states_buffer,
@@ -16,6 +17,12 @@ pub fn register_opcua_subscriptions(ros_node: Arc<Node>, opc_ua_client: Arc<Mute
     let joint_states_buffer_copy_position = Arc::clone(&joint_states_buffer);
     let joint_states_buffer_copy_velocity = Arc::clone(&joint_states_buffer);
     let joint_states_buffer_copy_effort = Arc::clone(&joint_states_buffer);
+
+    let tcp_pose_buffer = Arc::new(Mutex::new(TCPPoseBuffer::new(
+        ros_node_copy_tcp_pose_buffer,
+    )));
+    let tcp_pose_buffer_copy_pose = Arc::clone(&tcp_pose_buffer);
+    let tcp_pose_buffer_copy_quaternion = Arc::clone(&tcp_pose_buffer);
 
     opc_ua_client
         .lock()
@@ -60,6 +67,33 @@ pub fn register_opcua_subscriptions(ros_node: Arc<Node>, opc_ua_client: Arc<Mute
                     .lock()
                     .unwrap()
                     .on_effort_change(x);
+            },
+            0.0,
+        )
+        .expect("ERROR: Got an error while subscribing to variable");
+
+    opc_ua_client
+        .lock()
+        .unwrap()
+        .create_subscription(
+            1,
+            100707,
+            move |x| tcp_pose_buffer_copy_pose.lock().unwrap().on_pose_change(x),
+            0.0,
+        )
+        .expect("ERROR: Got an error while subscribing to variable");
+
+    opc_ua_client
+        .lock()
+        .unwrap()
+        .create_subscription(
+            1,
+            100710,
+            move |x| {
+                tcp_pose_buffer_copy_quaternion
+                    .lock()
+                    .unwrap()
+                    .on_quaternion_change(x)
             },
             0.0,
         )
