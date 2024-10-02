7059cc9c2c176f61035d69e48d4a011b25a08aa5
Philipp Caspers <117186241+philipp-caspers@users.noreply.github.com>
2024-10-02 13:05:16 +0000

    feat!: Use the new `joint_states` topic implementation
    meaning that from now on all fiels of the JoinStates message are filled
    with real data.

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,50 @@
 mod opc_ua_client;
 
 use env_logger::{Builder, Env};
-use log::{debug, info};
+use log::info;
 use opc_ua_client::OPCUAClient;
-use opcua::types::Variant;
 use rclrs::{create_node, Context, RclrsError};
+use register_subscriptions::register_opcua_subscriptions;
 use ros_services::ROSServices;
 use std::{
     env,
     sync::{Arc, Mutex},
 };
 
+mod register_subscriptions;
+mod ros_message_creation;
 mod ros_publisher;
 mod ros_services;
-
-use ros_publisher::{create_joint_state_msg, RosPublisher};
 
 fn main() -> Result<(), RclrsError> {
     Builder::from_env(Env::default().default_filter_or("info"))
         .filter_module("opcua", log::LevelFilter::Warn)
         .init();
     let context = Context::new(env::args()).unwrap();
-    let node = create_node(&context, "voraus_bridge_node")?;
-    let node_copy = Arc::clone(&node);
-    let joint_state_publisher = Arc::new(RosPublisher::new(&node, "joint_states").unwrap());
+    let ros_node = create_node(&context, "voraus_bridge_node")?;
+    let ros_node_copy_spin = Arc::clone(&ros_node);
+    let ros_node_copy_service = Arc::clone(&ros_node);
 
     let opc_ua_client = Arc::new(Mutex::new(OPCUAClient::new("opc.tcp://127.0.0.1:4855")));
     let Ok(_connection_result) = opc_ua_client.lock().unwrap().connect() else {
         panic!("Connection could not be established, but is required.");
     };
+    let opc_ua_client_copy_run = Arc::clone(&opc_ua_client);
+    let opc_ua_client_copy_services = Arc::clone(&opc_ua_client);
 
-    let ros_services = Arc::new(ROSServices::new(Arc::clone(&opc_ua_client)));
-    let _enable_impedance_control =
-        node_copy.create_service::<std_srvs::srv::Empty, _>("enable_impedance_control", {
+    register_opcua_subscriptions(ros_node, opc_ua_client);
+
+    let ros_services = Arc::new(ROSServices::new(opc_ua_client_copy_services));
+    let _enable_impedance_control = ros_node_copy_service
+        .create_service::<std_srvs::srv::Empty, _>("enable_impedance_control", {
             let rsc = Arc::clone(&ros_services);
             move |request_header, request| rsc.enable_impedance_control(request_header, request)
         });
 
-    let callback = {
-        let provider = Arc::clone(&joint_state_publisher);
-        move |x: Variant| {
-            debug!("Value = {:?}", &x);
-            let mut data_value: Vec<f64> = vec![];
-            match x {
-                Variant::Array(unwrapped) => {
-                    unwrapped.values.into_iter().for_each(|value| {
-                        let unpacked_value = value
-                            .as_f64()
-                            .expect("This should have been encapsulated f64 but wasn't.");
-                        data_value.push(unpacked_value)
-                    });
-                }
-                _ => panic!("Expected an array"),
-            };
-            let j_msg = create_joint_state_msg(data_value);
-            provider
-                .publish_data(&j_msg)
-                .expect("Error while publishing.")
-        }
-    };
-    opc_ua_client
-        .lock()
-        .unwrap()
-        .create_subscription(1, 100111, callback, 10.0)
-        .expect("ERROR: Got an error while subscribing to variables");
-    // Loops forever. The publish thread will call the callback with changes on the variables
     info!("Starting OPC UA client");
-    let _session = opc_ua_client.lock().unwrap().run_async();
+    let _session = opc_ua_client_copy_run.lock().unwrap().run_async();
     info!("Spinning ROS");
-    rclrs::spin(node_copy)
+    rclrs::spin(ros_node_copy_spin)
 }
 
 #[cfg(test)]
