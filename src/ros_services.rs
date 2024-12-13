3c54857b09c328dc73ba29ce9a921553d9fec6cb
Philipp Caspers <117186241+philipp-caspers@users.noreply.github.com>
2024-12-13 15:35:15 +0000

    feat: Add `move_joints` to `ROSServices`

--- src/ros_services.rs
+++ src/ros_services.rs
@@ -3,6 +3,7 @@ use std::sync::{Arc, Mutex};
 use log::info;
 use opcua::types::{NodeId, Variant};
 use std_srvs::srv::{Empty_Request, Empty_Response};
+use voraus_interfaces::srv::{MoveJoints_Request, MoveJoints_Response};
 
 use crate::opc_ua_client::OPCUAClient;
 
@@ -44,6 +45,35 @@ impl ROSServices {
             .unwrap()
             .call_method(object_id, method_id, None::<Vec<Variant>>);
         Empty_Response {
+            structure_needs_at_least_one_member: 0,
+        }
+    }
+
+    pub fn move_joints(
+        &self,
+        _request_header: &rclrs::rmw_request_id_t,
+        request: MoveJoints_Request,
+    ) -> MoveJoints_Response {
+        info!("ROS service call: Move Joints");
+        let object_id = NodeId::new(1, 100210);
+        let method_id = NodeId::new(1, 100211);
+        self.opc_ua_client.lock().unwrap().call_method(
+            object_id,
+            method_id,
+            Some(vec![
+                request.relative.into(),
+                request.target_reference_cs.into(),
+                request.target_coordinate.into(),
+                request.arriving_cs.into(),
+                request.velocity_scaling.into(),
+                request.with_blending.into(),
+                request.blending_parameter.into(),
+                request.config_vector.into(),
+                request.command_id.into(),
+                request.manual_mode.into(),
+            ]),
+        );
+        MoveJoints_Response {
             structure_needs_at_least_one_member: 0,
         }
     }
