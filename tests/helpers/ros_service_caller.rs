66408814dab4b9a00b8ea1fe4884e81d65179bab
Philipp Caspers <117186241+philipp-caspers@users.noreply.github.com>
2024-12-13 17:47:28 +0000

    test: Add generic service type support to the service caller

--- tests/helpers/ros_service_caller.rs
+++ tests/helpers/ros_service_caller.rs
@@ -5,32 +5,35 @@ use std::{
     time::Duration,
 };
 
-pub struct ServiceCaller {
-    pub client: Arc<rclrs::Client<std_srvs::srv::Empty>>,
+pub struct ServiceCaller<Srv>
+where
+    Srv: rosidl_runtime_rs::Service,
+{
+    pub client: Arc<rclrs::Client<Srv>>,
     pub node: Arc<Node>,
-    pub request: std_srvs::srv::Empty_Request,
+    pub request: Srv::Request,
     pub number_of_calls: Arc<Mutex<u32>>,
 }
 
-impl ServiceCaller {
+impl<Srv> ServiceCaller<Srv>
+where
+    Srv: rosidl_runtime_rs::Service,
+    Srv::Request: Default,
+{
     pub fn new(service: &str) -> Result<Arc<Self>, RclrsError> {
         let context = rclrs::Context::new(env::args())?;
-
         let node = rclrs::create_node(&context, "minimal_client")?;
+        let client = node.create_client::<Srv>(service)?;
 
-        let client = node.create_client::<std_srvs::srv::Empty>(service)?;
-
-        let request = std_srvs::srv::Empty_Request {
-            structure_needs_at_least_one_member: 0,
-        };
-        let action_caller = Arc::new(ServiceCaller {
+        let request = Srv::Request::default();
+        let service_caller = Arc::new(ServiceCaller {
             client,
             node,
             request,
             number_of_calls: Arc::new(Mutex::new(0)),
         });
 
-        Ok(action_caller)
+        Ok(service_caller)
     }
 
     pub fn start(&self) {
@@ -38,16 +41,14 @@ impl ServiceCaller {
             std::thread::sleep(std::time::Duration::from_millis(10));
         }
     }
+
     pub fn call(&self) {
         let num_calls_clone = Arc::clone(&self.number_of_calls);
         self.client
-            .async_send_request_with_callback(
-                &self.request,
-                move |_response: std_srvs::srv::Empty_Response| {
-                    let mut num_calls = num_calls_clone.lock().unwrap();
-                    *num_calls += 1;
-                },
-            )
+            .async_send_request_with_callback(&self.request, move |_response: Srv::Response| {
+                let mut num_calls = num_calls_clone.lock().unwrap();
+                *num_calls += 1;
+            })
             .unwrap();
 
         let timeout = Some(Duration::new(5, 0));
