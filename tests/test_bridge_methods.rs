fffbc5d4d72ae0cad49ed2c137095d1ee9b16d40
Philipp Caspers <117186241+philipp-caspers@users.noreply.github.com>
2024-12-13 15:20:23 +0000

    test: Assert that a service call actually reaches the OPC UA server
    Previously it was only asserted that the ROS service call was registered
    by the bridge but not if it was actually passed through to the OPC UA
    server.
    With this change, the assertion channel of the OPC UA test server is
    levaraged in order to cover more of the code path under test.

--- tests/test_bridge_methods.rs
+++ tests/test_bridge_methods.rs
@@ -1,4 +1,7 @@
-use std::sync::{mpsc, Arc};
+use std::{
+    sync::{mpsc, Arc},
+    time::Duration,
+};
 
 use common::{wait_for_function_to_pass, ManagedRosBridge};
 use helpers::opc_ua_test_server::OPCUATestServer;
@@ -8,10 +11,10 @@ pub mod helpers;
 
 #[test]
 fn e2e_ros_service_to_opc_ua_call() {
-    let (assertion_tx, _assertion_rx) = mpsc::channel();
+    let (assertion_tx, assertion_rx) = mpsc::channel();
     let _server = OPCUATestServer::new(assertion_tx);
 
-    let mut bridge_process = ManagedRosBridge::new(None).expect("Failed to start subprocess");
+    let mut _bridge_process = ManagedRosBridge::new(None).expect("Failed to start subprocess");
 
     let service_caller = Arc::new(
         helpers::ros_service_caller::ServiceCaller::new(
@@ -30,9 +33,15 @@ fn e2e_ros_service_to_opc_ua_call() {
         5000,
     )
     .unwrap();
-    bridge_process.terminate();
-    assert!(bridge_process
-        .get_std_err()
-        .unwrap()
-        .contains("ROS service called"));
+
+    wait_for_function_to_pass(
+        || {
+            let received = assertion_rx
+                .recv_timeout(Duration::from_millis(10))
+                .unwrap();
+            received.contains("impedance_control/enable")
+        },
+        5000,
+    )
+    .unwrap();
 }
