06735172dcf837dee28853062a3b9a236744c6f7
Philipp Caspers <117186241+philipp-caspers@users.noreply.github.com>
2024-12-13 14:12:24 +0000

    test: Rework the OPC UA test server
    such that it can be used for broad integration testing.
    This change also introduce a proper shutdown for the OPC UA
    server, such that the tests do not share the same OPC UA
    server instance.

--- tests/helpers/opc_ua_test_server.rs
+++ tests/helpers/opc_ua_test_server.rs
@@ -6,54 +6,128 @@ use opcua::server::prelude::{
 };
 use opcua::server::session::SessionManager;
 use opcua::sync::RwLock;
-use opcua::types::{CallMethodRequest, CallMethodResult, ObjectId, StatusCode};
+use opcua::types::{CallMethodRequest, CallMethodResult, ObjectId, StatusCode, Variant};
 use std::path::PathBuf;
+use std::sync::mpsc::Sender;
 use std::sync::Arc;
-use std::thread;
+use std::thread::{self, JoinHandle};
 
-pub async fn run_opc_ua_test_server() -> thread::JoinHandle<()> {
-    thread::spawn(|| {
-        opc_ua_test_server();
-    })
+pub struct OPCUATestServer {
+    server: Arc<RwLock<Server>>,
+    run_handle: Option<JoinHandle<()>>,
 }
 
-fn opc_ua_test_server() {
-    opcua::console_logging::init();
+impl OPCUATestServer {
+    pub fn new(assertion_tx: Sender<String>) -> OPCUATestServer {
+        let questionable_namespace = 1;
+        let mut server = Server::new(
+            ServerConfig::load(&PathBuf::from("tests/resources/opc_ua_test_server.conf")).unwrap(),
+        );
+        add_variables(&mut server, questionable_namespace);
+        add_methods(&mut server, questionable_namespace, assertion_tx);
+        let server = Arc::new(RwLock::new(server));
+        let server_cpy = Arc::clone(&server);
 
-    let mut server = Server::new(
-        ServerConfig::load(&PathBuf::from("tests/resources/opc_ua_test_server.conf")).unwrap(),
+        let run_handle = thread::spawn(move || {
+            Server::run_server(Arc::clone(&server_cpy));
+        });
+        OPCUATestServer {
+            server,
+            run_handle: Some(run_handle),
+        }
+    }
+}
+
+impl Drop for OPCUATestServer {
+    fn drop(&mut self) {
+        let server_cpy = self.server.clone();
+        server_cpy.try_write().unwrap().abort();
+        if let Some(run_handle) = self.run_handle.take() {
+            if let Err(err) = run_handle.join() {
+                eprintln!("Error while joining the run thread: {:?}", err);
+            }
+        }
+    }
+}
+
+fn add_variables(server: &mut Server, namespace: u16) {
+    add_variable_stub(
+        server,
+        NodeId::new(namespace, 100111),
+        "position".to_string(),
     );
-
-    let questionable_namespace = 1u16;
-
-    add_timed_variable(&mut server, questionable_namespace);
-    add_no_op_method(&mut server, questionable_namespace);
-
-    server.run();
+    add_variable_stub(
+        server,
+        NodeId::new(namespace, 100115),
+        "velocity".to_string(),
+    );
+    add_variable_stub(server, NodeId::new(namespace, 100113), "effort".to_string());
+    add_variable_stub(
+        server,
+        NodeId::new(namespace, 100707),
+        "tcp_pose".to_string(),
+    );
+    add_variable_stub(
+        server,
+        NodeId::new(namespace, 100710),
+        "tcp_quaternion".to_string(),
+    );
+    add_variable_stub(
+        server,
+        NodeId::new(namespace, 100708),
+        "tcp_twist".to_string(),
+    );
+    add_variable_stub(
+        server,
+        NodeId::new(namespace, 100711),
+        "tcp_wrench".to_string(),
+    );
+}
+fn add_methods(server: &mut Server, namespace: u16, assertion_tx: Sender<String>) {
+    add_method_stub(
+        server,
+        NodeId::new(namespace, 100182),
+        NodeId::new(namespace, 100263),
+        None,
+        None,
+        "impedance_control/enable".to_string(),
+        assertion_tx.clone(),
+    );
+    add_method_stub(
+        server,
+        NodeId::new(namespace, 100182),
+        NodeId::new(namespace, 100264),
+        None,
+        None,
+        "impedance_control/disable".to_string(),
+        assertion_tx.clone(),
+    );
 }
 
-fn add_timed_variable(server: &mut Server, namespace: u16) {
-    // These will be the node ids of the new variables
-    let ticks_since_launch_node_id = NodeId::new(namespace, 100111);
-
+/// Creates an OPC UA variable that is monotonously increasing.
+fn add_variable_stub(server: &mut Server, node_id: NodeId, identifier: String) {
     let address_space = server.address_space();
 
     // The address space is guarded so obtain a lock to change it
     {
         let mut address_space = address_space.write();
 
-        let rapid_folder_id = address_space
-            .add_folder("Rapid", "Rapid", &NodeId::objects_folder_id())
+        let joint_position_stub_folder_id = address_space
+            .add_folder(
+                identifier.clone(),
+                identifier.clone(),
+                &NodeId::objects_folder_id(),
+            )
             .unwrap();
 
         let _ = address_space.add_variables(
             vec![Variable::new(
-                &ticks_since_launch_node_id,
-                "joint_positions",
-                "joint_positions",
+                &node_id,
+                identifier.clone(),
+                identifier.clone(),
                 vec![0.0f64; 6],
             )],
-            &rapid_folder_id,
+            &joint_position_stub_folder_id,
         );
     }
 
@@ -63,7 +137,7 @@ fn add_timed_variable(server: &mut Server, namespace: u16) {
             let now = DateTime::now();
             let ticks_in_100_ns = now.ticks();
             let _ = address_space.set_variable_value(
-                ticks_since_launch_node_id.clone(),
+                node_id.clone(),
                 vec![ticks_in_100_ns as f64; 6],
                 &now,
                 &now,
@@ -72,39 +146,68 @@ fn add_timed_variable(server: &mut Server, namespace: u16) {
     }
 }
 
-fn add_no_op_method(server: &mut Server, namespace: u16) {
+fn add_method_stub(
+    server: &mut Server,
+    object_id: NodeId,
+    method_id: NodeId,
+    input_args: Option<Vec<Variant>>,
+    output_args: Option<Vec<Variant>>,
+    assertion_str: String,
+    assertion_tx: Sender<String>,
+) {
     let address_space = server.address_space();
     let mut address_space = address_space.write();
 
-    let object_id = NodeId::new(namespace, 100182);
-    ObjectBuilder::new(&object_id, "Functions", "Functions")
-        .event_notifier(EventNotifier::SUBSCRIBE_TO_EVENTS)
-        .organized_by(ObjectId::ObjectsFolder)
-        .insert(&mut address_space);
+    if !address_space.node_exists(&object_id) {
+        ObjectBuilder::new(&object_id, "Functions", "Functions")
+            .event_notifier(EventNotifier::SUBSCRIBE_TO_EVENTS)
+            .organized_by(ObjectId::ObjectsFolder)
+            .insert(&mut address_space);
+    }
 
-    // NoOp has 0 inputs and 0 outputs
-    let method_id = NodeId::new(namespace, 100263);
     MethodBuilder::new(&method_id, "NoOp", "NoOp")
         .component_of(object_id.clone())
-        .callback(Box::new(NoOp))
+        .callback(Box::new(NoOp {
+            assertion_str,
+            input_args,
+            output_args,
+            assertion_tx,
+        }))
         .insert(&mut address_space);
 }
 
-struct NoOp;
+struct NoOp {
+    assertion_str: String,
+    input_args: Option<Vec<Variant>>,
+    output_args: Option<Vec<Variant>>,
+    assertion_tx: Sender<String>,
+}
 
 impl callbacks::Method for NoOp {
     fn call(
         &mut self,
         _session_id: &NodeId,
         _session_map: Arc<RwLock<SessionManager>>,
-        _request: &CallMethodRequest,
+        request: &CallMethodRequest,
     ) -> Result<CallMethodResult, StatusCode> {
-        info!("NoOp method called");
+        match request.input_arguments == self.input_args {
+            true => {
+                info!("{}", self.assertion_str);
+                self.assertion_tx.send(self.assertion_str.clone()).unwrap()
+            }
+            false => {
+                panic!(
+                    "Unexpected input args. Expected {:?} but got {:?}",
+                    self.input_args, request.input_arguments
+                );
+            }
+        }
+
         Ok(CallMethodResult {
             status_code: StatusCode::Good,
             input_argument_results: None,
             input_argument_diagnostic_infos: None,
-            output_arguments: None,
+            output_arguments: self.output_args.clone(),
         })
     }
 }
